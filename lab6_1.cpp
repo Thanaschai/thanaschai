fcc2b0839f01c4d9f533d912a54af88d4bbfbc9c
Thanaschai <110957704+Thanaschai@users.noreply.github.com>
2022-12-09 16:45:27 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
+    int num1 = 0 ;
+    int num2 = 0 ;
+    int num = 1;
+    
+    while (num != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num ;
+
+    if (num != 0){
+
+        
+        if (num % 2 == 0){
+        num1++ ;
+    
+        }
+        else {num2++ ;
+        }      
+    }
+    }
+    cout << "#Even numbers = " << num1 << endl;
+    cout << "#Odd numbers = " << num2 << endl;
+
     return 0;
 }
